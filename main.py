e8624303f0c49cdbd41a23dc40898f203f977203
hieunlt <nguyenlethienhieu@gmail.com>
2020-08-06 10:10:30 +0700

    Refactor run_bot

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from swipe_bot.bot import TinderBot, BadooBot, OKCBot
 
 
 def run_bot(site):
-    bot_mapper = {"tinder": TinderBot, "okc": OKCBot, "badoo": BadooBot}
+    bot_switcher = {"tinder": TinderBot, "okc": OKCBot, "badoo": BadooBot}
     if site.lower() == "tinder":
         # in case the Facebook log in button does not directly appear
         while True:
             try:
-                bot = bot_mapper[site.lower()]()
+                bot = bot_switcher[site.lower()]()
                 bot.get_site()
                 bot.login()
                 break
@@ -21,7 +21,7 @@ def run_bot(site):
                 continue
     else:
         # things get simpler with Badoo and OKC
-        bot = bot_mapper[site.lower()]()
+        bot = bot_switcher[site.lower()]()
         bot.get_site()
         bot.login()
     bot.auto_swipe()
