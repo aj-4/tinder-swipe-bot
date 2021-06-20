3e03c255f941aba8185dd25a1690bdbc1b83381e
Tat Vi Quyen <tvq9612@gmail.com>
2021-06-20 11:11:30 +0700

    update 2006

--- tinder_bot.py
+++ tinder_bot.py
@@ -18,24 +18,35 @@ class TinderBot():
         sleep(3)
 
         cookie_btn = self.driver.find_element_by_xpath(
-            '//*[@id="content"]/div/div[2]/div/div/div[1]/button')
+            '//*[@id="q-84965404"]/div/div[2]/div/div/div[1]/button')
         cookie_btn.click()
 
+        btn_login = self.driver.find_element_by_xpath(
+            '//*[@id="q-84965404"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a')
+        btn_login.click()
+        sleep(1)
+
         try:
-            self.driver.find_element_by_xpath(
-                '//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
-        except:
             more_options = self.driver.find_element_by_xpath(
-                '//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/button')
-            more_options.click()
+                '//*[@id="q-1813346480"]/div/div/div[1]/div/div[3]/span/button')
+            if ('Trouble' not in more_options.text):
+                more_options.click()
+        except:
+            sleep(0.1)
         finally:
             fb_btn = self.driver.find_element_by_xpath(
-                '//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+                '//*[@id="q-1813346480"]/div/div/div[1]/div/div[3]/span/div[2]/button')
             fb_btn.click()
 
+        # more_options = self.driver.find_element_by_xpath(
+        #     '//*[@id="q-1813346480"]/div/div/div[1]/div/div[3]/span/button')
+        # more_options.click()
+
         # switch to login popup
+        sleep(2)
+        # return
         base_window = self.driver.window_handles[0]
-        self.driver.switch_to_window(self.driver.window_handles[1])
+        self.driver.switch_to.window(self.driver.window_handles[1])
 
         email_in = self.driver.find_element_by_xpath('//*[@id="email"]')
         email_in.send_keys(username)
@@ -43,23 +54,34 @@ class TinderBot():
         pw_in = self.driver.find_element_by_xpath('//*[@id="pass"]')
         pw_in.send_keys(password)
 
-        login_btn = self.driver.find_element_by_xpath('//*[@id="u_0_0"]')
+        # login_btn = self.driver.find_element_by_xpath('//*[@id="u_0_0_*"]')
+        login_btn = self.driver.find_element_by_xpath(
+            '//input[@type="submit"]')
         login_btn.click()
 
-        self.driver.switch_to_window(base_window)
+        self.driver.switch_to.window(base_window)
 
         sleep(6)
 
-        popup_1 = self.driver.find_element_by_xpath(
-            '//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
-        popup_1.click()
+        allow_location = self.driver.find_element_by_xpath(
+            '//*[@id="q-1813346480"]/div/div/div/div/div[3]/button[1]')
+        allow_location.click()
 
-        popup_2 = self.driver.find_element_by_xpath(
-            '//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
-        popup_2.click()
+        disable_noti = self.driver.find_element_by_xpath(
+            '//*[@id="q-1813346480"]/div/div/div/div/div[3]/button[2]')
+        disable_noti.click()
+
+        # popup_1 = self.driver.find_element_by_xpath(
+        #     '//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        # popup_1.click()
+
+        # popup_2 = self.driver.find_element_by_xpath(
+        #     '//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        # popup_2.click()
 
     def like(self):
         try:
+            # print('like')
             # Normal account has limited
             limited = self.driver.find_element_by_xpath(
                 '//*[@id="modal-manager"]/div/div/div[3]/button[2]')
@@ -68,7 +90,7 @@ class TinderBot():
                 return False
         except:
             like_btn = self.driver.find_element_by_xpath(
-                '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
+                '//*[@id="q-84965404"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div[2]/div[4]/button')
             like_btn.click()
         return True
 
